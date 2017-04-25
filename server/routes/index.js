a59d6f7eb581739bfd2cd2d541dab70d443f9fc9
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-04-25 11:09:43 -0700

    show insights
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Fabric Pane', insights: ['first', 'second'] });
+    res.render('index', { title: 'Fabric Pane', patientName: 'Jim Jones', insights: ['High risk for readmission', 'Due for mammogram'] });
 });
 
 module.exports = router;
