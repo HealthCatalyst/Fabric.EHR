c5cc14ab9fec49cbf286bd18f313daf1142fdde2
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-09-10 22:03:29 -0600

    update sql to create Insights->InsightItems hierarchy
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- sql/5-addsampledata.sql
+++ sql/5-addsampledata.sql
@@ -1,49 +1,147 @@
 
 TRUNCATE TABLE dbo.Insights;
+TRUNCATE TABLE dbo.InsightItems;
+TRUNCATE TABLE dbo.InsightLinks;
 TRUNCATE TABLE dbo.ControlledSubstances;
 TRUNCATE TABLE dbo.Registries;
 
 INSERT INTO [dbo].[Insights]
            ([PatientId]
-           ,[InsightId]
-		   ,[InsightsText]
-           ,[DateCreated]
+            ,[InsightId]
+            ,InsightCategory
+            , InsightName
+	      ,[InsightText]
+            ,[DateCreated]
 )
      VALUES
-           (1
-		   ,1
+           (1,1,
+           'Category'
            ,'High risk of opioid abuse'
-           ,GETDATE())
+           , ''
+           ,'04-25-2017')
+GO
+
+INSERT INTO [dbo].[InsightItems]
+           ([PatientId]
+            ,[InsightId]
+            ,[InsightItemId]
+	      ,[ItemName]
+	      ,[ItemText]
+            ,[DateCreated]
+)
+     VALUES
+           (1,1,1
+           ,'Daily Opioid MME Threshold'
+            , '50 (^ 28% in past 6 months)'
+           ,'04-25-2017')
+GO
+
+INSERT INTO [dbo].[InsightItems]
+           ([PatientId]
+            ,[InsightId]
+            ,[InsightItemId]
+	      ,[ItemName]
+	      ,[ItemText]
+            ,[DateCreated]
+)
+     VALUES
+           (1,1,2
+           ,'% Days Uses Opioid Over 50 MME' 
+            , '25.54% (22,845 days/ 89,446 days)'
+           ,'04-25-2017')
+GO
+
+INSERT INTO [dbo].[InsightItems]
+           ([PatientId]
+            ,[InsightId]
+            ,[InsightItemId]
+	      ,[ItemName]
+	      ,[ItemText]
+            ,[DateCreated]
+)
+     VALUES
+           (1,1,3
+           ,'% Patient Encounter Uses Opioid Over 50 MME' 
+            , '21.41% (9,284/43,371)'
+           ,'04-25-2017')
+GO
+
+INSERT INTO [dbo].[InsightItems]
+           ([PatientId]
+            ,[InsightId]
+            ,[InsightItemId]
+	      ,[ItemName]
+	      ,[ItemText]
+            ,[DateCreated]
+)
+     VALUES
+           (1,1,4
+           ,'Risk Factors' 
+            , '- Family History of Addiction, - Diagnosed Depression'
+           ,'04-25-2017')
+GO
+
+INSERT INTO [dbo].[InsightItems]
+           ([PatientId]
+            ,[InsightId]
+            ,[InsightItemId]
+	      ,[ItemName]
+	      ,[ItemText]
+            ,[DateCreated]
+)
+     VALUES
+           (1,1,5
+           ,'In Hospice Care?' 
+            , 'Started May 1, 2016'
+           ,'04-25-2017')
+GO
+
+INSERT INTO [dbo].[InsightLinks]
+           ([PatientId]
+            ,[InsightId]
+            ,[InsightLinkId]
+	      ,[LinkName]
+	      ,[LinkUrl]
+            ,[DateCreated]
+)
+     VALUES
+           (1,1,1
+           ,'Controlled Substance History' 
+           , ''
+           ,'04-25-2017')
 GO
 
 
 INSERT INTO [dbo].[Insights]
            ([PatientId]
-           ,[InsightId]
-		   ,[InsightsText]
-           ,[DateCreated]
+            ,[InsightId]
+            , InsightCategory
+            , [InsightName]
+	      ,[InsightText]
+            ,[DateCreated]
 )
      VALUES
-           (1
-		   ,2
+           (1,2
+           , 'Medication category'
            ,'Used controlled substance in past 90 days'
-           ,GETDATE())
+           , ''
+           ,'04-25-2017')
 GO
 
+/* add insights for other patients */
+INSERT INTO [dbo].[Insights]([PatientId],[InsightId], InsightCategory, [InsightName],[InsightText],[DateCreated])
+     VALUES (2,1,'Test Category','Mammogram overdue','text',GETDATE())
 
-INSERT INTO [dbo].[Insights]([PatientId],[InsightId],[InsightsText],[DateCreated])
-     VALUES (2,1,'Mammogram overdue',GETDATE())
+INSERT INTO [dbo].[Insights]([PatientId],[InsightId], InsightCategory, [InsightName],[InsightText],[DateCreated])
+     VALUES (3,1,'Registry Category','On Diabetes registry','text',GETDATE())
 
-INSERT INTO [dbo].[Insights]([PatientId],[InsightId],[InsightsText],[DateCreated])
-     VALUES (3,1,'On Diabetes registry',GETDATE())
-
-INSERT INTO [dbo].[Insights]([PatientId],[InsightId],[InsightsText],[DateCreated])
-     VALUES (3,2,'Alcoholic',GETDATE())
+INSERT INTO [dbo].[Insights]([PatientId],[InsightId], InsightCategory, [InsightName],[InsightText],[DateCreated])
+     VALUES (3,2, 'Risk Category','Alcoholic', 'text',GETDATE())
 
 
 --- controlled substances
-INSERT INTO dbo.ControlledSubstances(PatientId,MedicationName, DatePrescribed, PrescribedByDoctor)
-VALUES(1,'OxyContin', '3/1/2017', 'Dr. Bill Humphry');
+INSERT INTO dbo.ControlledSubstances(PatientId,ControlledSubstanceId, [MedicationCategory],MedicationName, DatePrescribed, PrescribedByDoctor)
+VALUES(1,1,'Opiod','OxyContin', '3/1/2017', 'Dr. Bill Humphry');
 
 
 INSERT INTO dbo.Registries(PatientId, RegistryName, DateAdded)
