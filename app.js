634c512fedabc826fb18cf50148493c82472c640
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-09-12 13:08:15 -0600

    move stuff from server folder to main folder so it can run on azure
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
-const express = require('express');
-const app = express();
-require('setimmediate');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+var insightspane = require('./routes/insightspane');
+var controlledsubstancepane = require('./routes/controlledsubstancepane');
+
+var app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.locals.moment = require('moment');
+
+process.argv.forEach(function(val, index, array) {
+    console.log(index + ': ' + val);
 });
 
-//app.use('/fhir-app', express.static('simple-fhir-app'));
-app.use('/fhir-app', express.static('smartonfhir/fhir-app'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-/* app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
-}); */
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-var myLogger = function(req, res, next) {
-    console.log(req);
-    next();
-};
+app.get('/', function(req, res) {
+    res.send('Fabric.EHR demo');
+});
+app.use('/fabricpane', insightspane);
+app.use('/controlledsubstancepane', controlledsubstancepane);
 
-app.use(myLogger);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-console.log('Listening on ' + app.get('port') + " env port=" + process.env.PORT);
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(app.get('port'));
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
