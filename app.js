7065139f54d7b1d7fefdd79aee0d340b6b91f652
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-09-21 14:41:27 -0700

    require setimmediate
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('setimmediate');
 
 var insightspane = require('./routes/insightspane');
 var controlledsubstancepane = require('./routes/controlledsubstancepane');
